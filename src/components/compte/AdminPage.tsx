0630b5d4554e5cb9a0ac7b8c1595d10b093c99fe
RudyDupuis <abcdupuisrudy@gmail.com>
2023-03-14 15:03:56 +0100

    remove componants in AdminPage

--- src/components/compte/AdminPage.tsx
+++ src/components/compte/AdminPage.tsx
@@ -7,14 +7,7 @@ const AdminPage = () => {
     <div className="admin-page">
       <h2>Posts à modérer</h2>
 
-      <Posts />
-      <Posts />
-
       <h2>Commentaires à modérer</h2>
-
-      <Comments />
-      <Comments />
-      <Comments />
     </div>
   );
 };
