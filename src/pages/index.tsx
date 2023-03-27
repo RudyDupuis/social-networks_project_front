55b48d174dd06a59da919dff7d53595487fc2f24
RudyDupuis <abcdupuisrudy@gmail.com>
2023-03-27 09:17:45 +0200

    finish login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -67,8 +67,10 @@ const index = () => {
 
       router.push("/accueil");
     } catch (error: any) {
-      if (error.response && error.response.status === 404) {
+      if (error.response && error.response.status === 401) {
         setErrorMessage("Votre compte n'existe pas");
+      } else {
+        setErrorMessage(`Une erreur ${error.response.status} s'est produite.`);
       }
     }
   };
